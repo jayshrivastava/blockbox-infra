ad42a1d970b18dd8517bfe48dd931c9fdcfa0f13
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-06-18 21:17:28 -0400

    Mongo DB Seed Script

---
+++ mongo-db-seed-script/README.md
@@ -0,0 +1,18 @@
+# What is this
+
+This loads data from the MovieLens dataset into Blockbox's MongoDB server. 
+This currently uses the 'small' 'latest' dataset:
+
+Small: 100,000 ratings and 3,600 tag applications applied to 9,000 movies by 600 users. Last updated 9/2018.
+
+# Setup
+
+Make sure you have pipenv installed. In the terminal run:
+
+1. `pipenv shell --python 3.7` (Creates a Venv)
+2. `pipenv install` (Installs Dependencies)
+3. `python main.py` (Runs the program)
+
+# Adding Dependencies
+
+From the pipenv shell, run `pipenv install <package>`
---
+++ mongo-db-seed-script/main.py
@@ -0,0 +1,97 @@
+import csv
+import json
+import pymongo
+from pymongo import MongoClient
+
+# Open files
+with open('data/movies.csv', 'r', encoding = 'utf-8-sig') as f:
+  reader = csv.reader(f)
+  movies = list(reader)
+  movies.pop(0)
+
+with open('data/ratings.csv', 'r', encoding = 'utf-8-sig') as f:
+  reader = csv.reader(f)
+  ratings = list(reader)
+  ratings.pop(0)
+
+# Get Environment Variables
+with open('env.json') as env_file:  
+    env = json.load(env_file)
+
+
+
+# Global Vars
+movies_dict = {} # movie id as key
+users_dict = {}       # user id as key
+oldest_movie_date = 1990
+client = MongoClient(env['MONGO_DB_URI']) # mongo DB client
+database = client['test']  # connect to test database
+users_collection = database['users_']
+movies_collection = database['movies_']
+
+# Populate Movies Dict
+for i, movie in enumerate(movies):
+    title = movie[1]
+    id = movie[0]
+
+    # Safely get the movie date. Add movie to movies_dict if the movie release date is greater or equal to oldest_movie_date
+    date_chunk = title[len(title)-8:len(title)]
+    tmp1 = date_chunk.split('(')
+    if (len(tmp1) > 1):
+        year = int(tmp1[1].split(')')[0])
+        if year >= oldest_movie_date:
+            movies_dict[id] = title
+
+# Init Movies Dict
+for i, movie in enumerate(movies):
+    title = movie[1]
+    id = movie[0]
+
+    # Safely get the movie date. Add movie to movies_dict if the movie release date is greater or equal to oldest_movie_date
+    date_chunk = title[len(title)-8:len(title)]
+    tmp1 = date_chunk.split('(')
+    if (len(tmp1) > 1):
+        year = int(tmp1[1].split(')')[0])
+        if year >= oldest_movie_date:
+            movies_dict[id] = {
+                "title": title
+            }
+   
+# Populate ratings in the movies dict and users dict IF the movie rated is in the movies dict
+for i, rating in enumerate(ratings):
+    movie_id = rating[1]
+    user_id = rating[0]
+    rating_val = rating[2]
+
+    # add rating to users dict
+    if movie_id in movies_dict:
+        if user_id not in users_dict:
+            users_dict[user_id] = []
+        
+        users_dict[user_id].append({ 
+            'movie_id': movie_id, 
+            'rating': float(rating_val)/5
+        })
+
+    # add ratings to movies dict
+    if movie_id in movies_dict:
+        if 'ratings' not in movies_dict[movie_id]:
+            movies_dict[movie_id]['ratings'] = []
+        movies_dict[movie_id]['ratings'].append({ 
+            'user_id': user_id, 
+            'rating': float(rating_val)/5
+        })        
+
+# Map Movies dict and users dict to arrays
+users_to_insert = [ { 'user_id': user_id, 'ratings': users_dict[user_id], 'name': '' } for user_id in users_dict.keys()]
+movies_to_insert = [] 
+for movie_id in movies_dict:
+    movie_to_insert = { 'movie_id': movie_id, 'title': movies_dict[movie_id]['title'] }
+    if 'ratings' in movies_dict[movie_id]:
+        movie_to_insert['ratings'] = movies_dict[movie_id]['ratings']
+    movies_to_insert.append(movie_to_insert)
+
+    
+# Insert movies and users into database
+result = users_collection.insert_many(users_to_insert)
+result = movies_collection.insert_many(movies_to_insert)
